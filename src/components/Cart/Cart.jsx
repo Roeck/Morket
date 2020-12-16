afbd9e8eb3339df26e48ee646a5c161b708ccdcf
Roeck <rons9n@gmail.com>
2020-12-16 18:01:01 -0500

    added routing

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 
@@ -8,7 +9,10 @@ const Cart = ({ cart }) => {
 
   const EmptyCart = () => (
     <Typography variant='subtitle1'>
-      Empty shopping cart. Start adding items!
+      Empty shopping cart.
+      <Link to='/' className={classes.link}>
+        Start adding items!
+      </Link>
     </Typography>
   );
 
