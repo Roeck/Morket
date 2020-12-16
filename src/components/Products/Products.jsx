f323cbdeca243c912cb51003d13d29a03c8dd0fe
Roeck <rons9n@gmail.com>
2020-12-15 19:35:44 -0500

    Product component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import Grid from '@material-ui/core';
 
 const products=[
-    {id: 1,name: "Laptop", description: "Windows Laptop"}
-    {id: 2,name: "Smartphone", description: "OnePlus 7T Pro"}
+    {id: 1,name: "Laptop", description: "Windows Laptop", price: '$400'}
+    {id: 2,name: "Smartphone", description: "OnePlus 7T Pro", price: '$900'}
 ]
 const Products = () => {
     <main>
         <Grid container justify="center" spacing={4}>
             {products.map((product => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product />
+                    <Product product={product} />
                 </Grid>
             )))}
         </Grid>
