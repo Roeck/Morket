4d40e2e957a14377fda510875ac070568f3c6169
Roeck <rons9n@gmail.com>
2020-12-16 15:48:17 -0500

    add to cart funcionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/logo.png';
 import useStyles from './styles';
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
   const classes = useStyles();
 
   return (
@@ -32,7 +32,7 @@ const Navbar = () => {
           <div className={classes.grow} />
           <div className={classes.button}>
             <IconButton aria-label='Show Cart Items' color='inherit'>
-              <Badge badgeContent={2} color='secondary'>
+              <Badge badgeContent={totalItems} color='secondary'>
                 <ShoppingCart />
               </Badge>
             </IconButton>
